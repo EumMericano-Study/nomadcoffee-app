f0111fb9d23a1919eb39c3f774374374e456d7e1
Eumericano <opwer032@naver.com>
2022-02-22 21:18:10 +0900

    fix: Change JsonStringfy to string

--- apollo.ts
+++ apollo.ts
@@ -7,8 +7,8 @@ export const tokenVar = makeVar("");
 
 export const logUserIn = async (token: string) => {
   await AsyncStorage.multiSet([
-    ["token", JSON.stringify(token)],
-    ["loggedIn", JSON.stringify("yes")],
+    ["token", token],
+    ["loggedIn", "yes"],
   ]);
   isLoggedInVar(true);
 };
