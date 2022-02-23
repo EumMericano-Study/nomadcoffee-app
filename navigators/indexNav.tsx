13bf8e8a3c0708c0a70e814e683b980aeb0200e7
Eumericano <opwer032@naver.com>
2022-02-23 09:03:17 +0900

    feat: Set common nav icons

--- navigators/indexNav.tsx
+++ navigators/indexNav.tsx
@@ -8,6 +8,7 @@ import Search from "../screens/Search";
 import LogIn from "../screens/LogIn";
 import Profile from "../screens/Profile";
 import Notification from "../screens/Notification";
+import TabIcon from "../components/nav/TabIcon";
 
 const Tabs = createBottomTabNavigator();
 
@@ -18,17 +19,59 @@ export default function LoggedOutNav() {
     <Tabs.Navigator
       screenOptions={{
         headerShown: false,
-        headerTintColor: "white",
+        tabBarActiveTintColor: "white",
+        tabBarShowLabel: false,
         tabBarStyle: {
           borderTopColor: "rgba(255,255,255,0.2)",
           backgroundColor: "black",
         },
       }}
     >
-      <Tabs.Screen name="Feed" component={Feed} />
-      <Tabs.Screen name="Search" component={Search} />
-      <Tabs.Screen name="Notification" component={Notification} />
-      <Tabs.Screen name="Profile" component={isLoggedIn ? Profile : LogIn} />
+      <Tabs.Screen
+        name="Feed"
+        component={Feed}
+        options={{
+          tabBarIcon: ({ focused, color, size }) => (
+            <TabIcon iconName="home" color={color} focused={focused} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="Search"
+        component={Search}
+        options={{
+          tabBarIcon: ({ focused, color, size }) => (
+            <TabIcon iconName="search" color={color} focused={focused} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="Camera"
+        component={Search}
+        options={{
+          tabBarIcon: ({ focused, color, size }) => (
+            <TabIcon iconName="camera" color={color} focused={focused} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="Notification"
+        component={Notification}
+        options={{
+          tabBarIcon: ({ focused, color, size }) => (
+            <TabIcon iconName="heart" color={color} focused={focused} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="Profile"
+        component={isLoggedIn ? Profile : LogIn}
+        options={{
+          tabBarIcon: ({ focused, color, size }) => (
+            <TabIcon iconName="person" color={color} focused={focused} />
+          ),
+        }}
+      />
     </Tabs.Navigator>
   );
 }
