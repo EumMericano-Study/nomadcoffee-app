90d8f05781fefa859ba7ea08b6376480d78a9b6a
Eumericano <opwer032@naver.com>
2022-02-24 08:51:50 +0900

    feat: Add feed gqls

--- apollo.ts
+++ apollo.ts
@@ -1,20 +1,45 @@
-import { ApolloClient, InMemoryCache, makeVar } from "@apollo/client";
+import {
+  ApolloClient,
+  createHttpLink,
+  InMemoryCache,
+  makeVar,
+} from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
+import AsyncStorageLib from "@react-native-async-storage/async-storage";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
 export const isLoggedInVar = makeVar(false);
 
 export const tokenVar = makeVar("");
 
+const TOKEN = "token";
+
 export const logUserIn = async (token: string) => {
-  await AsyncStorage.multiSet([
-    ["token", token],
-    ["loggedIn", "yes"],
-  ]);
+  await AsyncStorage.setItem(TOKEN, token);
   isLoggedInVar(true);
 };
 
-const client = new ApolloClient({
+export const logUserOut = async () => {
+  await AsyncStorageLib.removeItem(TOKEN);
+  isLoggedInVar(false);
+  tokenVar(undefined);
+};
+
+const httpLink = createHttpLink({
   uri: "https://nomad-sexy-coffee.herokuapp.com/graphql",
+});
+
+const authLink = setContext((_, { headers }) => {
+  return {
+    headers: {
+      ...headers,
+      token: tokenVar(),
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: httpLink,
   cache: new InMemoryCache(),
 });
 
--- screens/Feed.tsx
+++ screens/Feed.tsx
@@ -1,14 +1,40 @@
+import { gql, useQuery } from "@apollo/client";
 import React from "react";
 import styled from "styled-components/native";
+import { logUserOut } from "../apollo";
 import { color } from "../color";
 import { Props } from "../types";
 
+const FEED_QUERY = gql`
+  query seeCoffeeShops {
+    seeCoffeeShops {
+      name
+      user {
+        username
+        avatarURL
+      }
+      photos {
+        url
+      }
+      categories {
+        name
+        slug
+      }
+    }
+  }
+`;
+
 export default function Feed({ navigation }: Props) {
+  const { data } = useQuery(FEED_QUERY);
+  console.log(data);
   return (
     <Container>
       <CreateAccount>
         <CreateAccountText onPress={() => navigation.navigate("Profile")}>
           Feed
+        </CreateAccountText>
+        <CreateAccountText onPress={() => logUserOut()}>
+          Logout
         </CreateAccountText>
       </CreateAccount>
     </Container>
